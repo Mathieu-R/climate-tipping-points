9a41587c096902d037d8978dd7e8890a7267ce02
Mathieu-R <mathieu0709@gmail.com>
2021-02-22 20:11:58 +0100

    update readme

---
+++ .virtual_documents/notebooks/fold-hopf.ipynb.py
@@ -0,0 +1,50 @@
+
+
+
+import numpy as np
+from sympy import Matrix, symbols, init_printing, solve, lambdify
+import matplotlib.pyplot as plt
+
+init_printing()
+
+
+a1, a2, b1, b2, c1, c2, x, y, z, phi = symbols("a1 a2 b1 b2 c1 c2 x y z phi")
+
+
+def gamma(x):
+    return 0.12 * x + 0.21
+
+def f1(x):
+    return a1 * (x ** 3) + a2 * x + phi
+
+def f2(x, y, z):
+    return b1 * z + b2 * (gamma(x) - (y**2 + z**2)) * y
+
+def f3(x, y, z):
+    return c1 * y + c2 * (gamma(x) - (y**2 + z**2)) * z
+
+
+M = Matrix([f1(x), f2(x, y, z), f3(x, y, z)])
+
+
+fixed_points = solve(M, [x, y, z])[2]
+fixed_points
+
+
+M.jacobian([x, y, z])
+
+
+Jac = M.jacobian([x,y,z]).subs({x: fixed_points[0], y: fixed_points[1], z: fixed_points[2]})
+Jac
+
+
+Jac.charpoly()
+
+
+Jac.eigenvals()
+
+
+result = lambdify([a1, a2, b1, b2, c1, c2], J)
+
+
+
--- README.md
+++ README.md
@@ -8,15 +8,30 @@ Clone the project
 $ git clone https://github.com/Mathieu-R/climate-tipping-points.git
 ```
 
-Create virtual environment and install packages
+Create virtual environment
 ```bash
-$ python3 -m venv env
+$ python3 -m venv <env-name>
 $ source env/bin/activate
 $ python3 -m pip install --upgrade pip
+```
+
+Connect Jupyter to virtual environment
+```bash
+$ python3 -m pip3 install ipykernel
+$ python3 -m ipykernel install --name=<env-name>
+```
+
+Install required packages
+```bash
 $ python3 -m pip install --requirement=requirements.tex
 ```
 
-Launch project
+Launch Jupyter Lab
 ```bash
-$ python3 index.py
+$ jupyter lab
+```
+
+### Using Kite extension with Jupyter Lab
+```bash
+$ python3 -m pip install "jupyterlab-kite>=2.0.2"
 ```
